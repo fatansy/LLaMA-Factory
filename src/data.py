69c760dffd0e683ebfdd60d27b50277b3b80f6ae
fatansy <realbbbbai@qq.com>
2023-11-22 01:50:05 +0800

    data 处理完成

--- src/data.py
+++ src/data.py
@@ -1,49 +1,85 @@
 import json
 import re
 
-# 读取JSON文件
-with open('G:\\llm\\data\\AMO_chatting\\original.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
+def save_to_file(file_name, data):
+    result_file_path = 'G:\\llm\\data\\AMO_chatting\\' + file_name
+    with open(result_file_path, 'w', encoding='utf-8') as result_file:
+        json.dump(data, result_file, ensure_ascii=False, indent=4)
 
-# 存储对话数据的变量
-filtered_conversations = []
+    print(file_name + f" saved to {result_file_path}")
 
-# 遍历JSON数据
-for dialog in data:
-    messages = []
-    current_message = None  # To track consecutive messages of the same gender
-    for message in dialog:
-        # 1. 跳过包含特定词组的message
-        if any(phrase in message['消息内容'] for phrase in ["button : Try it", "depth_gift_msg", "depth gift after msg", "send gift :"]):
-            continue
 
-        # 2. 将图片或者链接替换为短语
-        message['消息内容'] = re.sub(r'http\S+', 'This is my photo.', message['消息内容'])
+def main():
+    # 读取JSON文件
+    with open('G:\\llm\\data\\AMO_chatting\\original_test.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
 
-        # 3. 合并连续同一性别发送的不重复message
-        if current_message and current_message['sender_gender'] == message['发送者性别']:
-            if message['消息内容'] not in current_message['content']:
-                current_message['content'] += f".{message['消息内容']}"
-        else:
-            if current_message:
-                messages.append(current_message)
+    # 存储对话数据的变量
+    filtered_conversations = []
+    multi_turn_conversations = []
 
-            current_message = {
-                'sender_gender': message['发送者性别'],
-                'content': message['消息内容'],
-            }
+    # 遍历JSON数据
+    for dialog in data:
+        messages = []
+        current_message = None  # To track consecutive messages of the same gender
+        for message in dialog:
+            # 1. 跳过包含特定词组的message
+            if any(phrase in message['消息内容'] for phrase in ["button : Try it", "depth_gift_msg", "depth gift after msg", "send gift :"]):
+                continue
 
-    # 补上最后一条
-    if current_message:
-        messages.append(current_message)
+            # 2. 将图片或者链接替换为短语
+            message['消息内容'] = re.sub(r'http\S+', 'This is my photo.', message['消息内容'])
 
-    # 4. 删除message数量少于5条的dialog
-    if len(messages) >= 10:
-        filtered_conversations.append(messages)
+            # 3. 合并连续同一性别发送的不重复message
+            if current_message and current_message['sender_gender'] == message['发送者性别']:
+                if message['消息内容'] not in current_message['content']:
+                    current_message['content'] += f".{message['消息内容']}"
+            else:
+                if current_message:
+                    messages.append(current_message)
 
-# 保存结果到result.json文件
-result_file_path = 'G:\\llm\\data\\AMO_chatting\\1_formated.json'
-with open(result_file_path, 'w', encoding='utf-8') as result_file:
-    json.dump(filtered_conversations, result_file, ensure_ascii=False, indent=2)
+                current_message = {
+                    'sender_gender': message['发送者性别'],
+                    'content': message['消息内容'],
+                }
 
-print(f"Filtered conversations saved to {result_file_path}")
+        # 补上最后一条
+        if current_message:
+            messages.append(current_message)
+
+        # 4. 删除message数量少于8条的dialog
+        if len(messages) >= 10:
+            # 5. 如果第一句是女性发的，去掉它
+            # 如果最后一句是男性发的，去掉它
+            if messages[0]['sender_gender'] == 'WOMAN':
+                messages.pop(0)
+            if messages[-1]['sender_gender'] == 'MAN':
+                messages.pop()
+            filtered_conversations.append(messages)
+
+            # 6. 转换为多轮对话数据格式
+            multi_turn_dialog = []
+            history = []
+            for i in range(1, len(messages), 2):
+                instruction = messages[i - 1]['content']
+                output = messages[i]['content']
+                if i>= 3:
+                    history.append([messages[i - 3]['content'], messages[i - 2]['content']])
+
+                multi_turn_dialog.append({
+                    "instruction": instruction,
+                    "input": "",
+                    "output": output,
+                    "history": history.copy()
+                })
+
+            multi_turn_conversations.extend(multi_turn_dialog)
+
+    # 保存中间结果到1_format.json文件
+    save_to_file('1_format.json', filtered_conversations)
+    # 保存最终结果到2_final.json文件
+    save_to_file('2_final.json', multi_turn_conversations)
+
+
+if __name__ == "__main__":
+    main()
