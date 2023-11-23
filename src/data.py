bcdcc6754148dfd0c552f046a34cf55e57150afd
jiacheng <jiacheng@yizhoucp.cn>
2023-11-23 19:24:59 +0800

    update data.py and some filter, and data v4

--- src/data.py
+++ src/data.py
@@ -1,5 +1,11 @@
 import json
 import re
+from datetime import datetime
+
+def convert_to_timestamp(date_string):
+    dt = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
+    timestamp = dt.timestamp()
+    return timestamp
 
 data_src_path = 'data/amo/'
 # data_src_path = 'G:\\llm\\data\\AMO_chatting\\'
@@ -30,15 +36,22 @@ def first_step(data_version):
 
     # 遍历JSON数据
     for dialog in data:
-        messages = []
+        result_messages = []
         current_message = None  # To track consecutive messages of the same gender
+        current_timestamp = 0
         for message in dialog:
-            # 截断超过40句
-            if len(messages) >= 40:
+            # 截断超过50句
+            if len(result_messages) >= 50:
                 break
 
-            # 跳过包含特定词组的message
-            if any(phrase in message['消息内容'] for phrase in ["button : Try it", "depth_gift_msg", "depth gift after msg", "send gift :", "Duration:", "Duur:", "Call not answered", "Call wasn't answered", "You've refused the call", "Refuse your call"]):
+            # 超过半天的接续对话，截断
+            if current_timestamp and convert_to_timestamp(message['发送时间']) - current_timestamp > 43200:
+                break
+            current_timestamp = convert_to_timestamp(message['发送时间'])
+
+            # 跳过包含特定词组(系统消息)的 message
+            # 跳过关于视频call的讨论
+            if any(phrase.lower() in message['消息内容'].lower() for phrase in ["button : Try it", "depth_gift_msg", "depth gift after msg", "send gift :", "Duration:", "Duur:", "Call not answered", "Call wasn't answered", "You've refused the call", "Refuse your call", "video call"]):
                 continue
 
             # 将图片或者链接替换为短语
@@ -46,7 +59,8 @@ def first_step(data_version):
 
             # 合并连续同一性别发送的不重复message
             if current_message and current_message['sender_gender'] == message['发送者性别']:
-                if message['消息内容'] not in current_message['content']:
+                # 内容被之前消息包含，直接丢弃，时间超过半小时不合并，直接丢弃
+                if message['消息内容'] not in current_message['content'] and convert_to_timestamp(message['发送时间']) - current_timestamp < 1800:
                     if current_message['content'].endswith('.') or current_message['content'].endswith('?'):
                         current_message['content'] += message['消息内容']
                     else:
@@ -54,7 +68,7 @@ def first_step(data_version):
                     
             else:
                 if current_message:
-                    messages.append(current_message)
+                    result_messages.append(current_message)
 
                 current_message = {
                     'sender_gender': message['发送者性别'],
@@ -63,17 +77,17 @@ def first_step(data_version):
 
         # 补上最后一条
         if current_message:
-            messages.append(current_message)
+            result_messages.append(current_message)
 
-        # 删除message数量少于8条的dialog
-        if len(messages) >= 10:
+        # 删除message数量少于20条的dialog
+        if len(result_messages) >= 20:
             # 如果第一句是女性发的，去掉它
             # 如果最后一句是男性发的，去掉它
-            if messages[0]['sender_gender'] == 'WOMAN':
-                messages.pop(0)
-            if messages[-1]['sender_gender'] == 'MAN':
-                messages.pop()
-            filtered_conversations.append(messages)
+            if result_messages[0]['sender_gender'] == 'WOMAN':
+                result_messages.pop(0)
+            if result_messages[-1]['sender_gender'] == 'MAN':
+                result_messages.pop()
+            filtered_conversations.append(result_messages)
 
     print(f'filtered_conversations: {len(filtered_conversations)}')
     # 保存中间结果到format.json文件
@@ -116,7 +130,7 @@ def second_step(data_version):
 
 if __name__ == "__main__":
 
-    data_version = 'v3'
+    data_version = 'v4'
 
     # 第一步，初步清理和格式化数据
     # first_step(data_version)
