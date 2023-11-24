7a916c785171439eedc57f5dd5883f8df1e81f1e
jiacheng <jiacheng@yizhoucp.cn>
2023-11-24 19:08:59 +0800

    update data format method

--- src/data.py
+++ src/data.py
@@ -7,17 +7,12 @@ def convert_to_timestamp(date_string):
     timestamp = dt.timestamp()
     return timestamp
 
-# data_src_path = 'data/amo/'
-data_src_path = "G:\\llm\\LLaMA-Factory\\data\\amo\\"
-
-def split_file(input_list, max_size = 15000):
-    return [input_list[i:i + max_size] for i in range(0, len(input_list), max_size)]
-
+data_src_path = 'data/amo/'
+# data_src_path = "G:\\llm\\LLaMA-Factory\\data\\amo\\"
 
 def read_from_file(file_name):
     with open(data_src_path + file_name, 'r', encoding='utf-8') as file:
         return json.load(file)
-
 
 def save_to_file(file_name, data):
     result_file_path = data_src_path + file_name
@@ -25,7 +20,6 @@ def save_to_file(file_name, data):
         json.dump(data, result_file, ensure_ascii=False, indent=4)
 
     print(file_name + f" saved to {result_file_path}")
-
 
 def first_step(data_version):
     # 读取JSON文件
@@ -39,6 +33,7 @@ def first_step(data_version):
         result_messages = []
         current_message = None  # To track consecutive messages of the same gender
         current_timestamp = 0
+        time_to_long = False
         for message in dialog:
             # 截断超过100句
             if len(result_messages) >= 100:
@@ -47,7 +42,6 @@ def first_step(data_version):
             # 超过半天的接续对话，截断
             if current_timestamp and convert_to_timestamp(message['发送时间']) - current_timestamp > 43200:
                 break
-            current_timestamp = convert_to_timestamp(message['发送时间'])
 
             # 跳过包含特定词组(系统消息)的 message
             # 跳过关于视频call的讨论
@@ -61,39 +55,49 @@ def first_step(data_version):
             # 合并连续同一性别发送的不重复message
             if current_message and current_message['sender_gender'] == message['发送者性别']:
                 # 内容被之前消息包含，直接丢弃，时间超过半小时不合并，直接丢弃
-                if message['消息内容'] not in current_message['content'] and convert_to_timestamp(message['发送时间']) - current_timestamp < 1800:
-                    if current_message['content'].endswith('.') or current_message['content'].endswith('?'):
-                        current_message['content'] += message['消息内容']
+                # 加上TODO标识，手工处理多句连续问题
+                if message['消息内容'] not in current_message['content']:
+                    if (convert_to_timestamp(message['发送时间']) - current_timestamp < 120) and not time_to_long:
+                        if current_message['content'].endswith('.') or current_message['content'].endswith('?'):
+                            current_message['content'] += message['消息内容']
+                        else:
+                            current_message['content'] += f".{message['消息内容']}"
                     else:
-                        current_message['content'] += f".{message['消息内容']}"
+                        time_to_long = True
                     
             else:
                 if current_message:
                     result_messages.append(current_message)
+                time_to_long = False
 
                 current_message = {
                     'sender_gender': message['发送者性别'],
                     'content': message['消息内容'],
                 }
 
+            current_timestamp = convert_to_timestamp(message['发送时间'])
+
         # 补上最后一条
         if current_message:
             result_messages.append(current_message)
 
         # 删除message数量少于20条的dialog
-        if len(result_messages) >= 20:
+        if len(result_messages) >= 22:
             # 如果第一句是女性发的，去掉它
-            # 如果最后一句是男性发的，去掉它
+            # 如果最后一句是男性发的，去掉它，如果最后一句是女性发的，去掉一组对话
             if result_messages[0]['sender_gender'] == 'WOMAN':
                 result_messages.pop(0)
-            if result_messages[-1]['sender_gender'] == 'MAN':
+
+            if result_messages[-1]['sender_gender'] == 'WOMAN':
+                result_messages.pop()
+                result_messages.pop()
+            else:
                 result_messages.pop()
             filtered_conversations.append(result_messages)
 
     print(f'filtered_conversations: {len(filtered_conversations)}')
     # 保存中间结果到format.json文件
     save_to_file(f'formated_{data_version}.json', filtered_conversations)
-
 
 def second_step(data_version):
 
@@ -123,19 +127,13 @@ def second_step(data_version):
 
         multi_turn_conversations.extend(multi_turn_dialog)
 
+    # 保存最终结果到2_final.json文件
     print(f'multi_turn_conversations: {len(multi_turn_conversations)}')
     save_to_file(f'final_{data_version}.json', multi_turn_conversations)
-    # 保存最终结果到2_final.json文件
-    multi_turn_conversations_splited = split_file(multi_turn_conversations)
-    i = 1
-    for _convs in multi_turn_conversations_splited:
-        save_to_file(f'final_{data_version}_{i}.json', _convs)
-        i += 1
-
 
 if __name__ == "__main__":
 
-    data_version = 'v5'
+    data_version = 'v6'
 
     # 第一步，初步清理和格式化数据
     # first_step(data_version)
